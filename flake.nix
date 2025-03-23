93b3cf265cb6a0ca37763ca9db2c51b28c1707ac
ryota2357 <noreply@ryota2357.com>
2025-03-23 12:27:01 +0900

    chore: setup flake nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            bison
+            flex
+            wabt
+          ];
+        };
+      }
+    );
+}
